e8399a22033ac72155650d539993d11160e3f36d
03PrakharSrivastava <127031677+03PrakharSrivastava@users.noreply.github.com>
2025-01-23 22:43:05 +0000

    Added login feature

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { User } from '../models/user.models.js';
 import uploadCloudinary from '../utils/cloudinary.js'
 import apiResponse from '../utils/apiResponse.js';
 
+const generateAccessandRefreshToken = async(userId)=>{
+ try {
+   const user = await User.findById(userId);
+   const accessToken = user.generateAccessToken();
+   const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({validateBeforeSave: false});
+
+   return {accessToken,refreshToken};
+
+ } catch (error) {
+   throw new ApiError(500,'Something went wrong');
+ }
+}
+
 const registerUser = asyncHandler(async(res,res)=>{
   const {username,email,fullname,password} = res.body;
 
@@ -54,4 +70,46 @@ return res.status(201).json(
 
 })
 
-export default registerUser;
+const loginUser = asyncHandler(async(req,res)=>{
+  const {username,password,email} = req.body;
+
+  if(!username || !email){
+    throw new ApiError(400,"Username or Email are required");
+  }
+
+  const user = await User.findOne({
+    $or: [{username},{email}]
+  });
+
+  if(!user){
+    throw new ApiError(400,"User doesn't exist");
+  }
+
+  const passwordChecking = await user.isPasswordCorrect(password);
+
+  if(!passwordChecking){
+    throw new ApiError(401,'Invalid Credentials');
+  }
+
+   const {accessToken,refreshToken} = await generateAccessandRefreshToken(user._id);
+   
+   const loggedIn = await User.findById(user._id).select('-password -refreshToken');
+  
+   const options = {
+    httpOnly: true,
+    secure: true
+   }
+
+   return res
+   .status(200)
+   .cookie("accessToken",accessToken,options)
+   .cookie("refreshToken",refreshToken,options)
+   .json(
+    new apiResponse(200,{
+      user: accessToken,loggedIn,refreshToken
+    },
+  "User logged In successfully")
+   );
+})
+
+export {registerUser,loginUser};
